f4d9993ad2501fc87a82cfd17d10da91e9d99fc2
Aisha Yusuffe <a.yusuff99@outlook.com>
2022-09-08 14:26:27 +0100

    Add addTask method to class and pass test

--- ToDo/app/src/main/java/todo/ToDo.java
+++ ToDo/app/src/main/java/todo/ToDo.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class ToDo {
     ArrayList<String> tasks = new ArrayList<String>();
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {}
+
+    public ArrayList<String> addTask(String task) {
+        tasks.add(task);
+        // return the array list with new task
+        return tasks;
     }
 }
--- ToDo/app/src/test/java/todo/ToDoTest.java
+++ ToDo/app/src/test/java/todo/ToDoTest.java
@@ -2,13 +2,29 @@ package todo;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class ToDoTest {
-    // it is initialised with an empty list of tasks.
-    @Test public void shouldIntialiseWithEmptyList() {
+    // It is initialised with an empty list of tasks.
+    @Test
+     public void shouldIntialiseWithEmptyList() {
         ToDo todo = new ToDo();
         assertTrue("Initial task list is empty", todo.tasks.isEmpty());
     }
-    
+    // It has a addTask method that takes a String argument and adds it to the list of tasks. 
+    @Test
+    public void shouldAddTasks() {
+        // 1. Instantiate the ToDo class
+        ToDo todo = new ToDo();
+        // 2. Create mock data that test will compare against
+        ArrayList<String> expectedList = new ArrayList<String>();
+        expectedList.add("Buy milk");
+
+        // 3. Write assertion statement
+        assertEquals("should add argument into task array list", expectedList, todo.addTask("Buy milk"));
+
+    }
+
 }
