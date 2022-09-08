b1eee3e65281309317dff11dc5e7419c54a473e0
Aisha Yusuffe <a.yusuff99@outlook.com>
2022-09-08 13:54:37 +0100

    Pass first test - class initialise with empty array list

---
+++ ToDo/app/src/main/java/todo/ToDo.java
@@ -0,0 +1,10 @@
+package todo;
+
+import java.util.ArrayList;
+
+public class ToDo {
+    ArrayList<String> tasks = new ArrayList<String>();
+    public static void main(String[] args) {
+        
+    }
+}
--- ToDo/app/src/test/java/todo/ToDoTest.java
+++ ToDo/app/src/test/java/todo/ToDoTest.java
@@ -1,9 +1,14 @@
-package ToDo;
+package todo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class ToDoTest {
+    // it is initialised with an empty list of tasks.
+    @Test public void shouldIntialiseWithEmptyList() {
+        ToDo todo = new ToDo();
+        assertTrue("Initial task list is empty", todo.tasks.isEmpty());
+    }
     
 }
