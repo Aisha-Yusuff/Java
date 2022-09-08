11cd0b35e640803472f22d0f5dc4af6f0c7c0311
Aisha Yusuffe <a.yusuff99@outlook.com>
2022-09-08 19:26:38 +0100

    Write first test and pass it using RGR

---
+++ Game/app/src/main/java/game/Game.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Game {
+    public static void main (String[] args) {}
+    
+    public String getsWordToGuess(){
+        String word = "B_____";
+        return word;
+    }
+}
